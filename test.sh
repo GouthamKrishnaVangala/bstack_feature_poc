99d2a4fd06ad79f9335311088f4a1cb00f03f989
GouthamKrishnaVangala <46326041+GouthamKrishnaVangala@users.noreply.github.com>
2021-05-14 12:14:00 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-curl https://api.github.com/repos/GouthamKrishnaVangala/bstack_feature_poc/contents/readme.md
+URL="https://api.github.com/repos/GouthamKrishnaVangala/bstack_feature_poc/contents/readme.md"
+response=$(curl -s -w "%{http_code}" $URL)
+http_code=$(tail -n1 <<< "$response")  # get the last line
+content=$(sed '$ d' <<< "$response")   # get all but the last line which contains the status code
 
-test=`echo '<b>TEST</b> <p align="center">  <a href="https://store.mosaiq.one/" target="_blank">      <img alt="mosaiq" src="https://mosaiq-ide-test-automation.s3.amazonaws.com/ide-test-automation/reports/badge/mosaiq-store-logo.png" width="150">  </a></p><h2><b> Mosaiq IDE Test Automation Status</b></h2><p align="center">  <a href="https://github.com/mosaiqone/ide-test-automation/actions?query=workflow%3Amosaiq-ide-test-automation">    <img alt="Overall Test Automation Status" src="https://github.com/mosaiqone/ide-test-automation/workflows/mosaiq-ide-test-automation/badge.svg"></a>  <a href=https://mosaiq-ide-test-automation.s3.amazonaws.com/ide-test-automation/reports/'$1'/319/allure-report/index.html target="_blank">    <img alt="Functional Test Status" src="https://mosaiq-ide-test-automation.s3.amazonaws.com/ide-test-automation/reports/badge/functionalTest.svg"></a>  <a href="https://github.com/mosaiqone/ide-test-automation">    <img alt="Performance Test Status" src="https://mosaiq-ide-test-automation.s3.amazonaws.com/ide-test-automation/reports/badge/performanceTest.svg"></a>  <a href="https://mosaiq-ide-test-automation.s3.amazonaws.com/ide-test-automation/reports/uiReports/report.html">    <img alt="Ui Responsiveness Test Status" src="https://mosaiq-ide-test-automation.s3.amazonaws.com/ide-test-automation/reports/badge/UiResponsiveTest.svg"></a></p>' | iconv -t UTF-8`
+echo "$http_code"
+echo "$content"
 
-# echo $test
+# test=`echo '<b>TEST</b> <p align="center">  <a href="https://store.mosaiq.one/" target="_blank">      <img alt="mosaiq" src="https://mosaiq-ide-test-automation.s3.amazonaws.com/ide-test-automation/reports/badge/mosaiq-store-logo.png" width="150">  </a></p><h2><b> Mosaiq IDE Test Automation Status</b></h2><p align="center">  <a href="https://github.com/mosaiqone/ide-test-automation/actions?query=workflow%3Amosaiq-ide-test-automation">    <img alt="Overall Test Automation Status" src="https://github.com/mosaiqone/ide-test-automation/workflows/mosaiq-ide-test-automation/badge.svg"></a>  <a href=https://mosaiq-ide-test-automation.s3.amazonaws.com/ide-test-automation/reports/'$1'/319/allure-report/index.html target="_blank">    <img alt="Functional Test Status" src="https://mosaiq-ide-test-automation.s3.amazonaws.com/ide-test-automation/reports/badge/functionalTest.svg"></a>  <a href="https://github.com/mosaiqone/ide-test-automation">    <img alt="Performance Test Status" src="https://mosaiq-ide-test-automation.s3.amazonaws.com/ide-test-automation/reports/badge/performanceTest.svg"></a>  <a href="https://mosaiq-ide-test-automation.s3.amazonaws.com/ide-test-automation/reports/uiReports/report.html">    <img alt="Ui Responsiveness Test Status" src="https://mosaiq-ide-test-automation.s3.amazonaws.com/ide-test-automation/reports/badge/UiResponsiveTest.svg"></a></p>' | iconv -t UTF-8`
 
-encod_data=`echo $test | base64 -w 0`
+# # echo $test
 
-# echo $encod_data
+# encod_data=`echo $test | base64 -w 0`
 
-curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $2" https://api.github.com/repos/GouthamKrishnaVangala/bstack_feature_poc/contents/readme.md -d '{"message": "testmessage1", "sha": "'"$3"'", "content": "'"$encod_data"'" }'
+# # echo $encod_data
 
-decode_data=`echo $encod_data | base64 --decode | iconv -f UTF-8`
+# curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $2" https://api.github.com/repos/GouthamKrishnaVangala/bstack_feature_poc/contents/readme.md -d '{"message": "testmessage1", "sha": "'"$3"'", "content": "'"$encod_data"'" }'
 
-echo $decode_data
+# decode_data=`echo $encod_data | base64 --decode | iconv -f UTF-8`
+
+# echo $decode_data
